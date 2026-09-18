4226eb6c6cba8626fb5972851951d7d190b17d33
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-09-18 22:41:22 +0500

    spotify

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const multer = require('multer')
 const authRoutes = require('./routes/auth.routes')
 const artistRoutes = require('./routes/artist.route')
 const app = express()
